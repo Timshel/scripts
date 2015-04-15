8974b6a47758873cebd143be4cbf302a602f16ea
Jacques <jacques.bachellerie@mfglabs.com>
2015-04-15 15:24:06 +0200

    always stay low res

--- xrandr_auto.sh
+++ xrandr_auto.sh
@@ -10,12 +10,11 @@ do
         if [ $nbactiv -eq 2 ] && [ $dp1Activ -eq 1 ] ;then
             echo Dual
             xfconf-query -c xsettings -p /Xft/DPI -s 90
-            xrandr --output DP1 --auto
             xrandr --output eDP1 --mode 1920x1200 --right-of DP1
         elif [ $nbactiv -eq 1 ]; then
             echo Simple
-            xfconf-query -c xsettings -p /Xft/DPI -s 150
-            xrandr --output eDP1 --auto            
+            xfconf-query -c xsettings -p /Xft/DPI -s 90
+            xrandr --output eDP1 --mode 1920x1200            
         fi
         if [ $lastPid -gt 0 ]; then
             kill $lastPid
