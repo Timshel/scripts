e1dd4079b578d9adca8544649440483a1667a8a3
Jacques <jacques.bachellerie@mfglabs.com>
2015-04-20 15:33:39 +0200

    remove the xmonad handling

--- xrandr_auto.sh
+++ xrandr_auto.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 last=0
-lastPid=0
 while true
 do
     sleep 1
     nbactiv=$(cat /sys/class/drm/card*-*/status | grep -e "^connected" | wc -l)
     dp1Activ=$(cat /sys/class/drm/card*-DP-1/status | grep -e "^connected" | wc -l)
     if [ $nbactiv -ne $last ];then
+        xfconf-query -c xsettings -p /Xft/DPI -s 90
         if [ $nbactiv -eq 2 ] && [ $dp1Activ -eq 1 ] ;then
             echo Dual
-            xfconf-query -c xsettings -p /Xft/DPI -s 90
+            xrandr --output DP1 --auto --primary
             xrandr --output eDP1 --mode 1920x1080 --right-of DP1
         elif [ $nbactiv -eq 1 ]; then
             echo Simple
-            xfconf-query -c xsettings -p /Xft/DPI -s 90
-            xrandr --output eDP1 --mode 1920x1080            
+            xrandr --output eDP1 --mode 1920x1080 --primary
         fi
         if [ $lastPid -gt 0 ]; then
             kill $lastPid
         fi
-        xmonad --replace&
-        lastPid=$!
-        trap "kill $lastPid; exit 1" SIGINT
     else
         sleep 1
     fi
