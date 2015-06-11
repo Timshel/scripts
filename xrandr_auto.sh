ddc084ee3884a7f83b92b70a088679ef5823d825
Jacques <jacques.bachellerie@mfglabs.com>
2015-06-11 17:05:13 +0200

    Some harcoded values for now

--- xrandr_auto.sh
+++ xrandr_auto.sh
@@ -4,19 +4,16 @@ while true
 do
     sleep 1
     nbactiv=$(cat /sys/class/drm/card*-*/status | grep -e "^connected" | wc -l)
-    dp1Activ=$(cat /sys/class/drm/card*-DP-1/status | grep -e "^connected" | wc -l)
+    dp1Activ=$(cat /sys/class/drm/card*-DP-*/status | grep -e "^connected" | wc -l)
     if [ $nbactiv -ne $last ];then
         xfconf-query -c xsettings -p /Xft/DPI -s 90
         if [ $nbactiv -eq 2 ] && [ $dp1Activ -eq 1 ] ;then
             echo Dual
-            xrandr --output DP1 --auto --primary
-            xrandr --output eDP1 --mode 1920x1080 --right-of DP1
+            xrandr --output DP1-8 --mode 3440x1440 --rate 50 --primary
+            xrandr --output eDP1 --mode 1920x1080 --right-of DP1-8
         elif [ $nbactiv -eq 1 ]; then
             echo Simple
             xrandr --output eDP1 --mode 1920x1080 --primary
-        fi
-        if [ $lastPid -gt 0 ]; then
-            kill $lastPid
         fi
     else
         sleep 1
